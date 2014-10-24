e0097bea9498c641644e15bb8aee029c71c742fd
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-10-24 18:46:24 +1100

    Part 1 Episode 3/Part 1 complete

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
-    @window.rootViewController = MainViewController.new
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(MainViewController.new)
 
     @window.backgroundColor = UIColor.grayColor
 
--- app/main_view_controller.rb
+++ app/main_view_controller.rb
@@ -1,15 +1,18 @@
 class MainViewController < UIViewController
   def loadView
+    self.title = "Tasks"
     self.view = UIView.new
+    view.backgroundColor = UIColor.whiteColor
 
-    label = UILabel.new
-    label.text = "Hello RubyMotion!"
-    label.frame = [[20, 100], [280, 30]]
-    view.addSubview(label)
+    button = UIButton.new
+    button.setTitle("Add Task", forState: UIControlStateNormal)
+    button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
+    button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
+    button.frame = [[20, 100], [280, 30]]
+    view.addSubview(button)
+  end
 
-    red_square = UIView.new
-    red_square.frame = [[135, 150], [50, 50]]
-    red_square.backgroundColor = UIColor.redColor
-    view.addSubview(red_square)
+  def add_task
+    navigationController.pushViewController(MainViewController.new, animated: true)
   end
 end
