6fc33c7add2bec0c2f10c68d37c2d42b0de3ac4e
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-10-30 07:59:38 +1100

    Preperation done

---
+++ app/add_task_view_controller.rb
@@ -0,0 +1,19 @@
+class AddTaskViewController < UIViewController
+  attr_accessor :task
+
+  def loadView
+    self.title = "Add Task"
+    self.view = UIView.new
+    view.backgroundColor = UIColor.whiteColor
+
+    @text_field = UITextField.new
+    @text_field.frame = [[20, 100], [280, 50]]
+    @text_field.backgroundColor = UIColor.lightGrayColor
+    @text_field.placeholder = "Enter your task"
+    view.addSubview(@text_field)
+  end
+
+  def viewWillDisappear(animated)
+    task.title = @text_field.text
+  end
+end
--- app/main_view_controller.rb
+++ app/main_view_controller.rb
@@ -1,18 +1,41 @@
 class MainViewController < UIViewController
+  attr_accessor :task
+
+  def init
+    super
+
+    self.task = Task.new
+
+    self
+  end
+
   def loadView
     self.title = "Tasks"
     self.view = UIView.new
     view.backgroundColor = UIColor.whiteColor
 
-    button = UIButton.new
-    button.setTitle("Add Task", forState: UIControlStateNormal)
-    button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
-    button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
-    button.frame = [[20, 100], [280, 30]]
-    view.addSubview(button)
+    @button = UIButton.new
+    @button.setTitle("Set Task", forState: UIControlStateNormal)
+    @button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
+    @button.frame = [[20, 100], [280, 30]]
+    view.addSubview(@button)
+
+    @label = UILabel.new
+    @label.frame = [[20, 150], [280, 30]]
+    view.addSubview(@label)
+  end
+
+  def viewDidLoad
+    @button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
+  end
+
+  def viewWillAppear(animated)
+    @label.text = task.title
   end
 
   def add_task
-    navigationController.pushViewController(MainViewController.new, animated: true)
+    vc = AddTaskViewController.new
+    vc.task = task
+    navigationController.pushViewController(vc, animated: true)
   end
 end
